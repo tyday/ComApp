f556ca278322106c85da00dd19a851f9dba7c3df
Ty Day <tyrday@gmail.com>
2018-04-17 13:15:45 -0400

    dramatically change layout

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,20 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function fncBtnDropClick() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
     }
+  }
 } 
