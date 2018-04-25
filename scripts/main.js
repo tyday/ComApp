48c679c35fa78e29b418c4a71c0dc78cab42a635
Ty Day <tyrday@gmail.com>
2018-04-24 21:18:47 -0400

    ADD: performance card creation

--- scripts/main.js
+++ scripts/main.js
@@ -44,9 +44,14 @@ if ('serviceWorker' in navigator) {
  * Events to download performer list
  * Set to happen after window loads to prevent slow load
  */
-window.addEventListener("load", getPerformerList());
+window.addEventListener("load", afterLoadEvents());
 
+function afterLoadEvents() {
+  getPerformerList();
+  // placePerformanceList();
+}
 function getPerformerList(){
+  //https://developers.google.com/web/updates/2015/03/introduction-to-fetch
   console.log('fetch started');
   fetch('api/findSchedule.php')
     .then(
@@ -59,6 +64,7 @@ function getPerformerList(){
         // Examine the text in the response
         response.json().then(function(data){
           console.log(data);
+          placePerformanceList(data);
         });
       }
     )
@@ -66,3 +72,55 @@ function getPerformerList(){
       console.log('Fetch Error :-S', err);
     }); 
 }
+
+
+function placePerformanceList(data){
+  var card;
+  var performerList = document.getElementById("performancelist")
+
+  card = createPerformanceCard('7:00 AM','Tuesday','Off-Ramp','T-Rex','Fictional,Delusional,Tempremental');
+  performerList.appendChild(card);
+
+  for(performance in data){
+    card = createPerformanceCard(performance.StartTime, performance.Day,performance.Stage,performance.Performer,performance.ThreeWordDesc);
+    performerList.appendChild(card);
+  }
+
+}
+function createPerformanceCard(vTime,vDay,vStage,vPerformer,vThreeWords){
+  var card = document.createElement("li");
+  card.className="slot-card";
+  var eleTime = document.createElement("div");
+  var subTime = document.createTextNode(vTime);
+  eleTime.appendChild(subTime);
+  eleTime.className="card-time";
+
+  var eleDay = document.createElement("div");
+  var subDay = document.createTextNode(vDay);
+  eleDay.appendChild(subDay);
+  eleDay.className="card-day";
+
+  var eleStage = document.createElement("div");
+  var subStage = document.createTextNode(vStage);
+  eleStage.appendChild(subStage);
+  eleStage.className="card-stage";
+
+  var elePerformer = document.createElement("div");
+  var subPerformer = document.createTextNode(vPerformer);
+  elePerformer.appendChild(subPerformer);
+  elePerformer.className="card-band";
+
+  var ele3Words = document.createElement("div");
+  var sub3Words = document.createTextNode(vThreeWords);
+  ele3Words.appendChild(sub3Words);
+  ele3Words.className="card-description";
+  
+  card.appendChild(eleTime);
+  card.appendChild(eleDay);
+  card.appendChild(eleStage);
+  card.appendChild(elePerformer);
+  card.appendChild(ele3Words);
+
+  return card;
+}
+
